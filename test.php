b6974456fe7b50684665abe44ea0c36e431e9da7
klove <klove@ac6037d2-46ff-0310-ae12-9beb5f4a4cfd>
2005-08-27 08:03:10 +0000

    initial commit
    git-svn-id: https://www.seasar.org/svn/s2container.php5/trunk@2 ac6037d2-46ff-0310-ae12-9beb5f4a4cfd

---
+++ test.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * Created on 2005/08/27
+ *
+ * To change the template for this generated file go to
+ * Window - Preferences - PHPeclipse - PHP - Code Templates
+ */
+ 
+print "test\n";
+ 
+?>
