8fa7461cfbcd187ae98ea18ae377624707cc82ce
nowel <nowel@ac6037d2-46ff-0310-ae12-9beb5f4a4cfd>
2005-11-30 07:07:42 +0000

    add extension/db/pdo
    git-svn-id: https://www.seasar.org/svn/s2container.php5/trunk@80 ac6037d2-46ff-0310-ae12-9beb5f4a4cfd

---
+++ src/s2container.php5/org/seasar/extension/db/pdo/S2Container_PDODataSource.class.php
@@ -0,0 +1,79 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+// +----------------------------------------------------------------------+
+// | PHP version 5                                                        |
+// +----------------------------------------------------------------------+
+// | Copyright 2004-2005 the Seasar Foundation and the Others.            |
+// +----------------------------------------------------------------------+
+// | Licensed under the Apache License, Version 2.0 (the "License");      |
+// | you may not use this file except in compliance with the License.     |
+// | You may obtain a copy of the License at                              |
+// |                                                                      |
+// |     http://www.apache.org/licenses/LICENSE-2.0                       |
+// |                                                                      |
+// | Unless required by applicable law or agreed to in writing, software  |
+// | distributed under the License is distributed on an "AS IS" BASIS,    |
+// | WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,                        |
+// | either express or implied. See the License for the specific language |
+// | governing permissions and limitations under the License.             |
+// +----------------------------------------------------------------------+
+// | Authors: klove                                                       |
+// |          nowel                                                       |
+// +----------------------------------------------------------------------+
+//
+// $Id$
+/**
+ * @package org.seasar.extension.db.pdo
+ * @author nowel
+ */
+class S2Container_PDODataSource extends S2Container_AbstractDataSource {
+
+    private $log_;
+    protected $dsn = "";
+    protected $option = "";
+
+    public function __construct(){
+        $this->log_ = S2Container_S2Logger::getLogger(__CLASS__);
+    }
+
+    public function setDsn($dsn){
+        $this->dsn = $dsn; 
+    }
+
+    public function setOption($option){
+        $this->option = $option;
+    }
+
+    public function getConnection(){
+        try {
+            if( !empty($this->option) ){
+                $this->dsn .= " " . $this->option;
+            }
+            if( !empty($this->user) || !empty($this->password) ){
+                $db = new PDO($this->dsn, $this->user, $this->password);
+            } else {
+                $db = new PDO($this->dsn);
+            }
+        } catch(PDOException $e){
+            $this->log_->error($e->getMessage(), __METHOD__);
+            $this->log_->error($e->getCode(), __METHOD__);
+            throw $e;
+        }
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $db;
+    }
+
+    public function disconnect($connection){
+        unset($connection);
+    }
+
+    public function __toString(){
+        $str .= 'user = ' . $this->user . ', ';
+        $str .= 'password = ' . $this->password . ', ';
+        $str .= 'dsn = ' . $this->dsn . ', ';
+        $str .= 'option = ' . $this->option;
+        return $str;
+    }
+
+}
+?>
