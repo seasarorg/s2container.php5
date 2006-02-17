31a568eb4b5f0be61f1afcff2b130fd0ac54d4ad
klove <klove@ac6037d2-46ff-0310-ae12-9beb5f4a4cfd>
2006-02-17 12:22:45 +0000

    git-svn-id: https://www.seasar.org/svn/s2container.php5/branches/annotation@162 ac6037d2-46ff-0310-ae12-9beb5f4a4cfd

--- examples/cache/cacheClient.php
+++ examples/cache/cacheClient.php
@@ -15,13 +15,13 @@ $PATH_A = EXAMPLE_DIR . "/cache/testA.dicon";
 
 
 $time_start = microtime_float();
-$container = S2ContainerCachingFactory::create($PATH_A);
+$container = S2ContainerFileCacheFactory::create($PATH_A);
 $time_end = microtime_float();
 $time = $time_end - $time_start;
 print "time : $time \n";
 
 $time_start = microtime_float();
-$container = S2ContainerCachingFactory::create($PATH_A);
+$container = S2ContainerFileCacheFactory::create($PATH_A);
 $time_end = microtime_float();
 $time = $time_end - $time_start;
 print "time : $time \n";
@@ -31,7 +31,7 @@ define('S2CONTAINER_PHP5_CACHE_DIR',EXAMPLE_DIR . "/cache/var");
 
 $PATH_B = EXAMPLE_DIR . "/cache/testB.dicon";
 $time_start = microtime_float();
-$container = S2ContainerCachingFactory::create($PATH_B);
+$container = S2ContainerFileCacheFactory::create($PATH_B);
 $time_end = microtime_float();
 $time = $time_end - $time_start;
 print "time : $time \n";
@@ -41,7 +41,7 @@ print_r($a);
 
 $PATH_C = EXAMPLE_DIR . "/cache/testC.dicon";
 $time_start = microtime_float();
-$container = S2ContainerCachingFactory::create($PATH_C,'testC.dicon');
+$container = S2ContainerFileCacheFactory::create($PATH_C,'testC.dicon');
 $time_end = microtime_float();
 $time = $time_end - $time_start;
 print "time : $time \n";
