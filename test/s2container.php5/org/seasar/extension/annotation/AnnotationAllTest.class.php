6e74a88015ca57a41bdf4527761f42ffd0b727c8
klove <klove@ac6037d2-46ff-0310-ae12-9beb5f4a4cfd>
2006-02-17 11:43:15 +0000

    git-svn-id: https://www.seasar.org/svn/s2container.php5/branches/annotation@157 ac6037d2-46ff-0310-ae12-9beb5f4a4cfd

--- test/s2container.php5/org/seasar/extension/annotation/AnnotationAllTest.class.php
+++ test/s2container.php5/org/seasar/extension/annotation/AnnotationAllTest.class.php
@@ -16,16 +16,7 @@ class AnnotationAllTest {
         $group->addTestCase(new CommentAnnotationReaderTests());
         $group->addTestCase(new AnnotationContainerTests());
         $group->addTestCase(new AnnotationsTests());
-        $group->addTestCase(new ClassPatternTests());
-        $group->addTestCase(new ClassTraversalTests());
-        $group->addTestCase(new AbstractAutoNamingTests());
-        $group->addTestCase(new DefaultAutoNamingTests());
-        $group->addTestCase(new FileSystemComponentAutoRegisterTests());
-        $group->addTestCase(new AbstractComponentAutoRegisterTests());
-        $group->addTestCase(new DefaultAnnotationHandlerTests());
-        $group->addTestCase(new AspectAutoRegisterTests());
-        $group->addTestCase(new InterfaceAspectAutoRegisterTests());
-        
+            
         return $group;    	
     }
 }
