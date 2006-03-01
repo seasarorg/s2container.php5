aeb9ca1ed9e4872359f3acf5c39f633a19c79e87
klove <klove@ac6037d2-46ff-0310-ae12-9beb5f4a4cfd>
2006-03-01 13:28:23 +0000

    git-svn-id: https://www.seasar.org/svn/s2container.php5/branches/annotation@191 ac6037d2-46ff-0310-ae12-9beb5f4a4cfd

--- examples/autoregister/autoregiseterClient.php
+++ examples/autoregister/autoregiseterClient.php
@@ -1,5 +1,5 @@
 <?php
-require_once('example.inc.php');
+require_once(dirname(dirname(__FILE__)) . '/example.inc.php');
 
 $PATH = EXAMPLE_DIR . "/autoregister/test.dicon";
 
--- examples/autoregister/greetingClient.php
+++ examples/autoregister/greetingClient.php
@@ -1,9 +1,9 @@
 <?php
-require_once('example.inc.php');
-require_once('classes/Greeting.class.php');
-require_once('classes/GreetingClient.class.php');
+require_once(dirname(dirname(__FILE__)) . '/example.inc.php');
+require_once('greeting/Greeting.class.php');
+require_once('greeting/GreetingClient.class.php');
 
-$PATH = EXAMPLE_DIR . "/autoregister/GreetingMain4.dicon";
+$PATH = EXAMPLE_DIR . "/autoregister/greeting.dicon";
 
 $container = S2ContainerFactory::create($PATH);
 $container->init();
