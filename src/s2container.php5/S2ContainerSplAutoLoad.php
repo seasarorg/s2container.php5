236b6de2b6bf88313f0cdefe043eefbd1a3c5f48
klove <klove@ac6037d2-46ff-0310-ae12-9beb5f4a4cfd>
2006-09-26 11:38:39 +0000

    add: S2ContainerSplAutoLoad.php
    git-svn-id: https://www.seasar.org/svn/s2container.php5/trunk@395 ac6037d2-46ff-0310-ae12-9beb5f4a4cfd

--- src/s2container.php5/S2ContainerSplAutoLoad.php
+++ src/s2container.php5/S2ContainerSplAutoLoad.php
@@ -25,7 +25,8 @@
  * @author klove
  */
 S2ContainerClassLoader::import(S2CONTAINER_PHP5);
-function __autoload($class = null){
-    S2ContainerClassLoader::load($class);
+if (function_exists('__autoload')) {
+    spl_autoload_register('__autoload');
 }
+spl_autoload_register(array('S2ContainerClassLoader', 'load'));
 ?>
