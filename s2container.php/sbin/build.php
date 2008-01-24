179a9366d56a603349c337bc58ea3acc87d88037
klove <klove@ac6037d2-46ff-0310-ae12-9beb5f4a4cfd>
2008-01-24 12:25:20 +0000

    git-svn-id: https://www.seasar.org/svn/s2container.php5/trunk@503 ac6037d2-46ff-0310-ae12-9beb5f4a4cfd

---
+++ s2container.php/sbin/build.php
@@ -0,0 +1,119 @@
+<?php
+define('ROOT_DIR', dirname(dirname(__FILE__)));
+define('SEASAR_ROOT_DIR', ROOT_DIR);
+require_once(ROOT_DIR . '/classes/seasar/util/ClassLoader.php');
+seasar::util::ClassLoader::$CLASSES = array();
+seasar::util::ClassLoader::import(ROOT_DIR . DIRECTORY_SEPARATOR . 'classes' . DIRECTORY_SEPARATOR . 'seasar', $namespace = array('seasar'));
+seasar::util::ClassLoader::import(ROOT_DIR . DIRECTORY_SEPARATOR . 'classes' . DIRECTORY_SEPARATOR . 'legacy', $namespace = array(), true);
+$coreClasses = getCoreClasses();
+genCoreFile($coreClasses);
+setupClassLoader($coreClasses);
+
+exit;
+
+/** FUNCTIONS */
+function setupClassLoader(array $coreClasses) {
+    $contents = 'seasar::util::ClassLoader::$CLASSES = array(' . PHP_EOL;
+    $classes = array();
+    foreach(seasar::util::ClassLoader::$CLASSES as $className => $filePath) {
+        if (in_array($className, $coreClasses)) {
+            continue;
+        }
+        $filePath = str_replace(ROOT_DIR, '', $filePath);
+        $filePath = str_replace(DIRECTORY_SEPARATOR, '/', $filePath);
+        $classes[] = '    ' . "'$className' => SEASAR_ROOT_DIR . '$filePath'";
+    }
+    $contents .= implode(',' . PHP_EOL, $classes);
+    $contents .= ');' . PHP_EOL;
+
+    $classLoaderContents = file_get_contents(ROOT_DIR . '/classes/seasar/util/ClassLoader.php');
+    $classLoaderContents = preg_replace('/}[^}]+$/', '', $classLoaderContents);
+
+    $classLoaderContents .= '}' . PHP_EOL . PHP_EOL . $contents;
+    //print $classLoaderContents;
+    file_put_contents(ROOT_DIR . '/classes/seasar/util/ClassLoader.php', $classLoaderContents);
+}
+
+function genCoreFile(array $coreClasses) {
+    $coreSrc = array();
+    foreach($coreClasses as &$coreClass) {
+        $coreClass = trim($coreClass);
+        $classFile = ROOT_DIR . '/classes/' . preg_replace('/::/', DIRECTORY_SEPARATOR, $coreClass) . '.php';
+        $coreSrc = array_merge($coreSrc, file($classFile));
+    }
+
+    $src = '';
+    foreach($coreSrc as $line) {
+        if (preg_match('/^\/\//', trim($line))) {
+            continue;
+        }
+        $line = preg_replace('/^<\?php/s', '', $line);
+        $line = preg_replace('/\?>/s', '', $line);
+        if (trim($line) === '') {
+            continue;
+        }
+        $src .= $line;
+    }
+    $src = preg_replace('/\/\*.+?\*\//s', '', $src);
+    $src = '<?php' . PHP_EOL . $src;
+    file_put_contents(ROOT_DIR . '/S2ContainerCore.php', $src);
+}
+
+function getCoreClasses() { return array(
+'seasar::exception::S2RuntimeException',
+'seasar::log::S2Logger',
+'seasar::log::LoggerFactory',
+'seasar::log::impl::SimpleLoggerFactory',
+'seasar::log::impl::SimpleLogger',
+'seasar::container::S2ApplicationContext',
+'seasar::util::Annotation',
+'seasar::container::S2Container',
+'seasar::container::impl::S2ContainerImpl',
+'seasar::container::util::MetaDefSupport',
+'seasar::container::ComponentDef',
+'seasar::container::impl::SimpleComponentDef',
+'seasar::beans::AbstractPropertyDesc',
+'seasar::beans::AccessorMethodPropertyDesc',
+'seasar::beans::PublicPropertyDesc',
+'seasar::beans::BeanDesc',
+'seasar::beans::BeanDescFactory',
+'seasar::util::StringUtil',
+'seasar::container::impl::ComponentDefImpl',
+'seasar::container::util::ArgDefSupport',
+'seasar::container::util::PropertyDefSupport',
+'seasar::container::util::InitMethodDefSupport',
+'seasar::container::util::AspectDefSupport',
+'seasar::container::InstanceDef',
+'seasar::container::deployer::InstanceSingletonDef',
+'seasar::container::deployer::InstancePrototypeDef',
+'seasar::container::deployer::InstanceDefFactory',
+'seasar::container::AutoBindingDef',
+'seasar::container::assembler::AutoBindingAutoDef',
+'seasar::container::assembler::AutoBindingNoneDef',
+'seasar::container::assembler::AutoBindingDefFactory',
+'seasar::util::ClassUtil',
+'seasar::container::deployer::AbstractComponentDeployer',
+'seasar::container::deployer::SingletonComponentDeployer',
+'seasar::container::assembler::AbstractAssembler',
+'seasar::container::assembler::ManualConstructorAssembler',
+'seasar::container::assembler::AutoConstructorAssembler',
+'seasar::container::assembler::ManualPropertyAssembler',
+'seasar::container::assembler::AutoPropertyAssembler',
+'seasar::container::assembler::AbstractMethodAssembler',
+'seasar::container::assembler::InitMethodAssembler',
+'seasar::container::util::ConstructorUtil',
+'seasar::container::factory::S2ContainerFactory',
+'seasar::container::factory::S2ContainerBuilder',
+'seasar::container::factory::XmlS2ContainerBuilder',
+'seasar::container::impl::TooManyRegistrationComponentDef',
+'seasar::aop::Pointcut',
+'seasar::container::impl::ArgDef',
+'seasar::container::impl::AspectDef',
+'seasar::container::util::AopUtil',
+'seasar::aop::S2AopFactory',
+'seasar::aop::Aspect',
+'seasar::util::EvalUtil',
+'seasar::aop::MethodInterceptor',
+'seasar::aop::EnhancedClassGenerator'
+);}
+
---
+++ s2container.php/sbin/pear-pkg.php
@@ -0,0 +1,73 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<package packagerversion="1.5.0" version="2.0" xmlns="http://pear.php.net/dtd/package-2.0" xmlns:tasks="http://pear.php.net/dtd/tasks-1.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://pear.php.net/dtd/tasks-1.0     http://pear.php.net/dtd/tasks-1.0.xsd     http://pear.php.net/dtd/package-2.0     http://pear.php.net/dtd/package-2.0.xsd">
+ <name>S2Container</name>
+ <channel>pear.php.net</channel>
+ <summary>S2Container.PHP5, a lightweight container supports DI and AOP.</summary>
+ <description>S2Container is a lightweight container which supports DI(Dependency Injection) and AOP(Aspect Oriented Programming). The goal of S2Container.PHP5 project is to port Java Seasar2 to PHP5.</description>
+ <lead>
+  <name>klove</name>
+  <user>klove</user>
+  <email>s2container-php5@ml.seasar.org</email>
+  <active>yes</active>
+ </lead>
+ <developer>
+  <name>nowel</name>
+  <user>nowel</user>
+  <email>s2container-php5@ml.seasar.org</email>
+  <active>yes</active>
+ </developer>
+ <date>2008-01-01</date>
+ <time>00:00:00</time>
+ <version>
+  <release>2.0.0RC1</release>
+  <api>2.0.0RC1</api>
+ </version>
+ <stability>
+  <release>beta</release>
+  <api>beta</api>
+ </stability>
+ <license uri="http://www.apache.org/licenses/LICENSE-2.0">The Apache License, Version 2.0</license>
+ <notes>Release 2.0.0RC1</notes>
+ <contents>
+  <dir baseinstalldir="S2Container" name="/">
+<?php
+    define('ROOT_DIR', dirname(dirname(__FILE__)));
+    $ret = array();
+    $path = ROOT_DIR . DIRECTORY_SEPARATOR;
+    $r_ite = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($path));
+    foreach ($r_ite as $ite) {
+        if (!$ite->isFile() or 
+            preg_match('/^\./', $ite->getFileName()) or
+            preg_match('/\.svn/i', $ite->getRealPath()) ) {
+            continue;
+        }
+        $item = str_replace($path, '', $ite->getRealPath());
+        $item = str_replace(DIRECTORY_SEPARATOR, '/', $item);
+        if (!preg_match('/^classes/', $item) and
+            !preg_match('/^S2Container/', $item) and
+            !preg_match('/^Apache/', $item)) {
+            continue;
+        }
+
+        if (preg_match('/\.tgz/', $item)) {
+            continue;
+        }
+
+        print "    <file role=\"php\" name=\"$item\" />" . PHP_EOL;
+    }
+?>
+  </dir>
+ </contents>
+ <dependencies>
+  <required>
+   <php>
+    <min>5.2.0</min>
+   </php>
+   <pearinstaller>
+    <min>1.5.0</min>
+   </pearinstaller>
+  </required>
+ </dependencies>
+ <phprelease />
+ <changelog />
+</package>
