f22065d81bb8533c53125bb4f6d8968ecaabe6af
klove <klove@ac6037d2-46ff-0310-ae12-9beb5f4a4cfd>
2008-01-24 12:25:20 +0000

    git-svn-id: https://www.seasar.org/svn/s2container.php5/trunk/s2container.php@503 ac6037d2-46ff-0310-ae12-9beb5f4a4cfd

---
+++ sbin/unit.php
@@ -0,0 +1,50 @@
+<?php
+system('php build.php');
+
+define('ROOT_DIR', dirname(dirname(__FILE__)));
+
+if (!defined("PHPUnit_MAIN_METHOD")) {
+    define("PHPUnit_MAIN_METHOD", "");
+}
+require_once('PHPUnit/TextUI/TestRunner.php');
+require_once(ROOT_DIR . '/test/test.inc.php');
+
+$suite = new PHPUnit_Framework_TestSuite('Unit Test');
+$testDir = ROOT_DIR . '/test';
+isset($argv[1]) ? $targetPattern = $argv[1] : $targetPattern = '.*Test';
+$testClasses = array();
+find_test($testDir, $testClasses, $namespace = array());
+foreach($testClasses as $testFile => $testClass) {
+    if (!preg_match('/' . $targetPattern . '/', $testFile)) {
+        continue;
+    }
+    require_once($testFile);
+    $classRef = new ReflectionClass($testClass);
+    if ($classRef->isAbstract() or 
+        $classRef->isInterface() or 
+        !$classRef->isSubclassOf(new ReflectionClass('PHPUnit_Framework_TestCase'))) {
+        continue;
+    }
+    $suite->addTest(new PHPUnit_Framework_TestSuite($classRef));
+}
+PHPUnit_TextUI_TestRunner::run($suite);
+
+function find_test($dirPath, &$spool, $namespace) {
+    $iterator = new DirectoryIterator($dirPath);
+    while($iterator->valid()) {
+        if ($iterator->isDot()) {
+            $iterator->next();
+            continue;
+        }
+        if ($iterator->isFile()) {
+            $matches = array();
+            if (preg_match('/^([^\.]+?Test)\..*php$/', $iterator->getFileName(), $matches)) {
+                $className = implode('::', array_merge($namespace, (array)$matches[1]));
+                $spool[$iterator->getRealPath()] = $className;
+            }
+        } else if ($iterator->isDir()) {
+            find_test($iterator->getRealPath(), $spool, array_merge($namespace, (array)$iterator->getFileName()));
+        }
+        $iterator->next();
+    }
+}
