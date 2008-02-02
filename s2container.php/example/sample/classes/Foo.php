1dde711a1851716bc00de034ff571fd276b2051d
klove <klove@ac6037d2-46ff-0310-ae12-9beb5f4a4cfd>
2008-02-02 09:37:37 +0000

    git-svn-id: https://www.seasar.org/svn/s2container.php5/trunk@515 ac6037d2-46ff-0310-ae12-9beb5f4a4cfd

--- s2container.php/example/sample/classes/Foo.php
+++
@@ -1,6 +0,0 @@
-<?php
-class Foo {
-    public function setHoge(Hoge $hoge) {
-        $this->hoge = $hoge;
-    }
-}
--- s2container.php/example/sample/classes/aa/bb/Bar.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace aa::bb;
-class Bar {
-    /**
-     * @S2Binding('Hoge')
-     */
-    public $hoge = null;
-}
