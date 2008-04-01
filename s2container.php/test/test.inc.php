01b9efe1bfdcf7e167bfd3cfedc10a9edab7034e
klove <klove@ac6037d2-46ff-0310-ae12-9beb5f4a4cfd>
2008-04-01 12:56:39 +0000

    git-svn-id: https://www.seasar.org/svn/s2container.php5/trunk@536 ac6037d2-46ff-0310-ae12-9beb5f4a4cfd

--- s2container.php/test/test.inc.php
+++ s2container.php/test/test.inc.php
@@ -1,4 +1,7 @@
 <?php
+error_reporting(E_ALL | E_STRICT);
+date_default_timezone_set('Asia/Tokyo');
+
 require_once(dirname(__FILE__) . '/../S2Container.php');
 seasar::Config::$DEBUG_EVAL = true;
 
