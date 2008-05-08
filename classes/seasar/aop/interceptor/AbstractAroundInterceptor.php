037f65a2f1c7eec34b4ad86742cf644b30fc2702
klove <klove@ac6037d2-46ff-0310-ae12-9beb5f4a4cfd>
2008-05-08 15:01:20 +0000

    git-svn-id: https://www.seasar.org/svn/s2container.php5/trunk/s2container.php@537 ac6037d2-46ff-0310-ae12-9beb5f4a4cfd

--- classes/seasar/aop/interceptor/AbstractAroundInterceptor.php
+++ classes/seasar/aop/interceptor/AbstractAroundInterceptor.php
@@ -15,7 +15,7 @@
 // | governing permissions and limitations under the License.             |
 // +----------------------------------------------------------------------+
 /**
- * ArroundインターセプタのAbstractクラス
+ * AroundインターセプタのAbstractクラス
  *
  * @copyright 2005-2008 the Seasar Foundation and the Others.
  * @license   http://www.apache.org/licenses/LICENSE-2.0
