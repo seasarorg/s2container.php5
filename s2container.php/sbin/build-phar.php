b8e45a85407c57c9075832a92dc7398758f55983
klove <klove@ac6037d2-46ff-0310-ae12-9beb5f4a4cfd>
2008-05-31 10:48:53 +0000

    git-svn-id: https://www.seasar.org/svn/s2container.php5/trunk@579 ac6037d2-46ff-0310-ae12-9beb5f4a4cfd

---
+++ s2container.php/sbin/build-phar.php
@@ -0,0 +1,52 @@
+<?php
+$ROOT_DIR   = dirname(dirname(__FILE__));
+$PHAR_NAME  = 'S2Container';
+$VERSION    = '2.0.0RC1';
+$PHAR_FILE  = dirname(dirname(__FILE__)) . "/$PHAR_NAME-$VERSION.phar";
+$PHAR_ALIAS = $PHAR_NAME;
+
+if (file_exists($PHAR_FILE)) {
+    unlink($PHAR_FILE);
+}
+
+$phar = new Phar($PHAR_FILE, 0, $PHAR_ALIAS);
+//$phar->buildFromDirectory(dirname(dirname(__FILE__)) . '/classes', '/^[^\.])/i');
+
+$path = $ROOT_DIR . DIRECTORY_SEPARATOR;
+$r_ite = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($path));
+foreach ($r_ite as $ite) {
+    if (!$ite->isFile() or 
+        preg_match('/^\./', $ite->getFileName()) or
+        preg_match('/\.svn/i', $ite->getRealPath()) ) {
+        continue;
+    }
+    $item = str_replace($path, '', $ite->getRealPath());
+    $item = str_replace(DIRECTORY_SEPARATOR, '/', $item);
+    if (!preg_match('/^classes/', $item) and
+        !preg_match('/^S2Container/', $item) and
+        !preg_match('/^Apache/', $item)) {
+        continue;
+    }
+
+    if (preg_match('/(\.tgz|\.phar)/', $item)) {
+        continue;
+    }
+
+    $phar->addFile($ite->getRealPath(), $item);
+    print "addFile {$ite->getRealPath()} as $item" . PHP_EOL;
+}
+
+$stub = <<< EOSTUB
+<?php
+    Phar::mapPhar('$PHAR_NAME');
+    define("S2CONTAINER_PHAR", true);
+    require_once("phar://$PHAR_NAME/S2Container.php");
+    __HALT_COMPILER();
+EOSTUB;
+//print $stub;
+$phar->setStub($stub);
+
+//$phar = new Phar($PHAR_FILE);
+//$phar->extractTo(dirname(__FILE__) . '/tmp');
+
+
--- s2container.php/sbin/pear-pkg.php
+++ s2container.php/sbin/pear-pkg.php
@@ -2,8 +2,8 @@
 <package packagerversion="1.5.0" version="2.0" xmlns="http://pear.php.net/dtd/package-2.0" xmlns:tasks="http://pear.php.net/dtd/tasks-1.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://pear.php.net/dtd/tasks-1.0     http://pear.php.net/dtd/tasks-1.0.xsd     http://pear.php.net/dtd/package-2.0     http://pear.php.net/dtd/package-2.0.xsd">
  <name>S2Container</name>
  <channel>pear.php.net</channel>
- <summary>S2Container.PHP5, a lightweight container supports DI and AOP.</summary>
- <description>S2Container is a lightweight container which supports DI(Dependency Injection) and AOP(Aspect Oriented Programming). The goal of S2Container.PHP5 project is to port Java Seasar2 to PHP5.</description>
+ <summary>S2Container.PHP, a lightweight container supports DI and AOP.</summary>
+ <description>S2Container is a lightweight container which supports DI(Dependency Injection) and AOP(Aspect Oriented Programming). The goal of S2Container.PHP project is to port Java Seasar2 to PHP5.</description>
  <lead>
   <name>klove</name>
   <user>klove</user>
@@ -16,8 +16,8 @@
   <email>s2container-php5@ml.seasar.org</email>
   <active>yes</active>
  </developer>
- <date>2008-01-01</date>
- <time>00:00:00</time>
+ <date><?php echo date('Y-m-d');?></date>
+ <time><?php echo date('H:i:s');?></time>
  <version>
   <release>2.0.0RC1</release>
   <api>2.0.0RC1</api>
@@ -49,7 +49,7 @@
             continue;
         }
 
-        if (preg_match('/\.tgz/', $item)) {
+        if (preg_match('/(\.tgz|\.phar)/', $item)) {
             continue;
         }
 
@@ -61,7 +61,7 @@
  <dependencies>
   <required>
    <php>
-    <min>5.2.0</min>
+    <min>5.3.0dev</min>
    </php>
    <pearinstaller>
     <min>1.5.0</min>
