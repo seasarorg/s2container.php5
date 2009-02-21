98363784016d98e88af5ad7a21264078cf71b90a
klove <klove@ac6037d2-46ff-0310-ae12-9beb5f4a4cfd>
2009-02-21 15:06:04 +0000

    git-svn-id: https://www.seasar.org/svn/s2container.php5/trunk/s2container.php@608 ac6037d2-46ff-0310-ae12-9beb5f4a4cfd

--- example/misc/pdo/classes/sample/pdo/CdDao_findBySqlFile4.sql
+++
@@ -1,5 +0,0 @@
-select * from CD
-/* <?php if ($condition->id[0] !== null):?> */
-    where id = /*:condition_id_0*/5 or
-          id = /*:condition_id_0*/1
-/* <?php endif?> */
--- example/misc/pdo/classes/sample/pdo/ICdDao.php
+++
@@ -1,5 +0,0 @@
-<?php
-namespace sample\pdo;
-interface ICdDao {
-    public function findAll();
-}
