ec7208d1c268867a935d4e0173a33fc004eb24e7
klove <klove@ac6037d2-46ff-0310-ae12-9beb5f4a4cfd>
2009-02-21 15:06:04 +0000

    git-svn-id: https://www.seasar.org/svn/s2container.php5/trunk@608 ac6037d2-46ff-0310-ae12-9beb5f4a4cfd

--- s2container.php/example/misc/pdo/classes/sample/pdo/CdDao_findBySqlFile2.sql
+++
@@ -1,4 +0,0 @@
-select * from CD
-/* <?php if ($condition->id() !== null):?> */
-    where id = /*:condition_id*/5
-/* <?php endif?> */
--- s2container.php/example/misc/pdo/classes/sample/pdo/CdDao_findBySqlFile3.sql
+++
@@ -1,4 +0,0 @@
-select * from CD
-/* <?php if (is_integer($condition['id'])):?> */
-    where id = /*:condition_id*/5
-/* <?php endif?> */
