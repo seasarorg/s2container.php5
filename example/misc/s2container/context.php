2d13d85cd41df9f46a9577b66bfbb2a76caf7cfc
klove <klove@ac6037d2-46ff-0310-ae12-9beb5f4a4cfd>
2009-02-26 11:30:52 +0000

    git-svn-id: https://www.seasar.org/svn/s2container.php5/trunk/s2container.php@614 ac6037d2-46ff-0310-ae12-9beb5f4a4cfd

--- example/misc/s2container/context.php
+++ example/misc/s2container/context.php
@@ -1,8 +1,7 @@
 <?php
 require_once(dirname(dirname(dirname(__FILE__))) . '/example.inc.php');
-
-\seasar\container\S2ApplicationContext::import(dirname(__FILE__) . '/classes');
-$container = \seasar\container\S2ApplicationContext::create();
-$service    = $container->getComponent('Service');
+use seasar\container\S2ApplicationContext as s2app;
+s2app::import(dirname(__FILE__) . '/classes');
+$service = s2app::get('Service');
 
 print get_class($service->container) . PHP_EOL;
