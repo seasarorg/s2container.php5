d63802e3d321691561440d7d0ca5913ddf30c8d6
klove <klove@ac6037d2-46ff-0310-ae12-9beb5f4a4cfd>
2009-10-12 06:46:11 +0000

    git-svn-id: https://www.seasar.org/svn/s2container.php5/trunk@699 ac6037d2-46ff-0310-ae12-9beb5f4a4cfd

--- s2zf193/application/configs/s2.php
+++ s2zf193/application/configs/s2.php
@@ -3,7 +3,7 @@ require_once('S2Container.php');
 use \seasar\container\S2ApplicationContext as s2app;
 
 /** S2Log設定 */
-\seasar\Config::$LOG_LEVEL = seasar\log\impl\SimpleLogger::DEBUG;
+\seasar\Config::$LOG_LEVEL = seasar\log\impl\SimpleLogger::WARN;
 \seasar\Config::$DEBUG_EVAL = true;
 \seasar\Config::$DEBUG_VERBOSE = false;
 \seasar\Config::$SIMPLE_LOG_FILE = dirname(APPLICATION_PATH) . '/var/logs/s2.log';
