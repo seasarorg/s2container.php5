a9f9d93872d98aedb11b28d970071a9451514ff8
klove <klove@ac6037d2-46ff-0310-ae12-9beb5f4a4cfd>
2009-12-24 17:52:27 +0000

    git-svn-id: https://www.seasar.org/svn/s2container.php5/trunk@734 ac6037d2-46ff-0310-ae12-9beb5f4a4cfd

--- s2zf196/application/dicons/index/add2.php
+++ s2zf196/application/dicons/index/add2.php
@@ -8,14 +8,6 @@
  *   - @var string $moduleDir モジュールディレクトリパス
  */
 
-/**
- * S2ActionHelperでコンポーネントを取得する際に、コンポーネント名で取得する場合は、
- * あらかじめ、コンポーネントをインポートする必要があります。
- * コンポーネントのインポートを、S2ApplicationContextのimportメソッドで行う場合は、
- * クラスの@S2Componentアノテーションでコンポーネント名を設定します。
- */
-//use \seasar\container\S2ApplicationContext as s2app;
-//s2app::import("$moduleDir/services", 'Service', false, true);
 
 /**
  * コンポーネント登録をs2component関数で実施する場合は、次のようにコンポーネント名を設定します。
