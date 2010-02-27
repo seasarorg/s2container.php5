72ed9d78fad8923951ec3f78155617fbc477e665
klove <klove@ac6037d2-46ff-0310-ae12-9beb5f4a4cfd>
2010-02-27 13:42:38 +0000

    git-svn-id: https://www.seasar.org/svn/s2container.php5/trunk/s2container.php@746 ac6037d2-46ff-0310-ae12-9beb5f4a4cfd

--- classes/seasar/beans/BeanDesc.php
+++ classes/seasar/beans/BeanDesc.php
@@ -1,6 +1,6 @@
 <?php
 // +----------------------------------------------------------------------+
-// | Copyright 2005-2009 the Seasar Foundation and the Others.            |
+// | Copyright 2005-2010 the Seasar Foundation and the Others.            |
 // +----------------------------------------------------------------------+
 // | Licensed under the Apache License, Version 2.0 (the "License");      |
 // | you may not use this file except in compliance with the License.     |
@@ -20,7 +20,7 @@
  *   - セッターメソッドを管理します。
  *   - タイプヒントされたセッターメソッドを管理します。
  *
- * @copyright 2005-2009 the Seasar Foundation and the Others.
+ * @copyright 2005-2010 the Seasar Foundation and the Others.
  * @license   http://www.apache.org/licenses/LICENSE-2.0
  * @link      http://s2container.php5.seasar.org/
  * @version   SVN: $Id:$
--- classes/seasar/container/exception/CyclicReferenceRuntimeException.php
+++ classes/seasar/container/exception/CyclicReferenceRuntimeException.php
@@ -1,6 +1,6 @@
 <?php
 // +----------------------------------------------------------------------+
-// | Copyright 2005-2009 the Seasar Foundation and the Others.            |
+// | Copyright 2005-2010 the Seasar Foundation and the Others.            |
 // +----------------------------------------------------------------------+
 // | Licensed under the Apache License, Version 2.0 (the "License");      |
 // | you may not use this file except in compliance with the License.     |
@@ -21,7 +21,7 @@
  * コンポーネントのコンストラクタ引数に、 同じコンポーネントを指定した場合などに発生します。
  * </p>
  * 
- * @copyright 2005-2009 the Seasar Foundation and the Others.
+ * @copyright 2005-2010 the Seasar Foundation and the Others.
  * @license   http://www.apache.org/licenses/LICENSE-2.0
  * @link      http://s2container.php5.seasar.org/
  * @version   SVN: $Id:$
