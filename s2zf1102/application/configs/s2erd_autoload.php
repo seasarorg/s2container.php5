b61b3ce3f27e49d14710662bac87b9b0bcc2ec5e
klove <klove@ac6037d2-46ff-0310-ae12-9beb5f4a4cfd>
2010-02-28 04:09:47 +0000

    git-svn-id: https://www.seasar.org/svn/s2container.php5/trunk@751 ac6037d2-46ff-0310-ae12-9beb5f4a4cfd

--- s2zf1102/application/configs/s2erd_autoload.php
+++ s2zf1102/application/configs/s2erd_autoload.php
@@ -1,8 +1,5 @@
 <?php
 require_once('S2Erd.php');
-
-s2init();
-
 require_once(dirname(__FILE__) . '/s2erd.php');
 s2component('seasar\erd\writer\DynamicWriter');
 s2component('seasar\erd\util\zend\Autoloader');
